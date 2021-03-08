3c72d06337fb3b5000e97f09f830c8fd5f34cf35
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2021-03-07 22:11:05 -0300

    Creating views Edit and Create to class Service

--- BudgetIT/Controllers/ServicosController.cs
+++ BudgetIT/Controllers/ServicosController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BudgetIT.Models;
 using BudgetIT.Services;
+using BudgetIT.Services.Exception;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BudgetIT.Controllers
@@ -21,5 +23,61 @@ namespace BudgetIT.Controllers
             var list = _ServicoService.FindAll();
             return View(list);
         }
+
+        //Metodo Get Serviço 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Metodo post Servico
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Servico Servico)
+        {
+            _ServicoService.Insert(Servico);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Metodo get Servico
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                NotFound();
+            }
+            var obj = _ServicoService.FindById(id.Value);
+            if(obj == null)
+            {
+                NotFound();
+            }
+            return View(obj);
+        }
+
+        //Metodo post Servico
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Servico servico)
+        {
+            if(id != servico.Id)
+            {
+                return NotFound();
+            }
+            try
+            {
+                _ServicoService.Update(servico);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+               return NotFound();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+               return BadRequest();
+            }
+
+        }
+    
     }
 }
--- BudgetIT/Services/ServicoService.cs
+++ BudgetIT/Services/ServicoService.cs
@@ -1,4 +1,5 @@
 ﻿using BudgetIT.Models;
+using BudgetIT.Services.Exception;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,10 +15,38 @@ namespace BudgetIT.Services
             _context = context;
         }
 
-
         public List<Servico> FindAll()
         {
            return _context.Servico.ToList();
+        }
+
+        public void Insert(Servico obj)
+        {
+            _context.Servico.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Servico FindById(int id)
+        {
+           return _context.Servico.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Update(Servico obj)
+        {
+            if(! _context.Servico.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Servico.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbUpdateConcurrencyException(e.Message);
+            }
+
         }
     }
 }
